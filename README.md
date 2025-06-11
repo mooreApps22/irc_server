ed34194bc79409c19d6110ab4d30ad0554fab817
mooreApps22 <124527392+mooreApps22@users.noreply.github.com>
2025-06-11 17:52:19 +0100

    Setting up some basic program argument handling.

--- README.md
+++ README.md
@@ -2,4 +2,37 @@ This is a IRC server build with C++ 98. (Work in process.)
 
 Subject Notes: 
 
+External Functions:
+
+socket,
+close,
+setsockopt,
+getsockname,
+getprotobyname,
+gethostbyname,
+getaddrinfo,
+freeaddrinfo,
+bind,
+connect,
+listen,
+accept,
+htons,
+htonl,
+ntohs,
+ntohl,
+inet_addr,
+inet_ntoa,
+send,
+recv,
+signal,
+sigaction,
+lseek,
+fstat,
+fcntl,
+poll(or equivalent)
+
+DO NOT:
+	develop an IRC client (just the server)
+	implement server-to-server comm.
+
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include "../inc/data.hpp"
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	std::cout << "hello ircserv" << std::endl;
+	try	
+	{
+		if (ac != 3)
+			throw std::runtime_error("Usage: ./ircserv <port> <password>");
+		std::string port = av[1];
+		std::string password = av[2];
+		std::cout << "Port: " << port << std::endl;
+		std::cout << "Password: " << password << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return (1);
+	}
 	return (0);
 }
