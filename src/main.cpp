4132fee3bb8ca56ff1fa2abef7b12926a655ab8f
mooreApps22 <124527392+mooreApps22@users.noreply.github.com>
2025-06-11 18:31:39 +0100

    Took notes on the subject.

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@ int	main(int ac, char **av)
 	{
 		if (ac != 3)
 			throw std::runtime_error("Usage: ./ircserv <port> <password>");
-		std::string port = av[1];
-		std::string password = av[2];
+		std::string port = av[1]; // port # of IRC server which listens for incoming IRC connections 
+		std::string password = av[2]; // The connection password, the IRC client needs it to connect to server
 		std::cout << "Port: " << port << std::endl;
 		std::cout << "Password: " << password << std::endl;
 	}
