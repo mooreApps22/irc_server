e8bbbd93cf8c65e57dd0b8b65a30c630c3b7abf8
mooreApps22 <124527392+mooreApps22@users.noreply.github.com>
2025-06-11 17:38:30 +0100

    Just setting up environment.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is a IRC server build with C++ 98. (Work in process.)
+
+Subject Notes: 
+
+
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "../inc/data.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	std::cout << "hello ircserv" << std::endl;
+	return (0);
+}
