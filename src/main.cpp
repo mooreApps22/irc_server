4b4780537c0f3def2030283e053366845fa371cc
mooreApps22 <124527392+mooreApps22@users.noreply.github.com>
2025-06-12 16:24:16 +0100

    Playing around with a client connection.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,16 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <cstring>
+#include <netdb.h>
+#include <errno.h>
+#include <stdio.h>
+
+/*
+	Server Terminal: ./ircserv 6667 password
+	
+	Client Terminal: nc 127.0.0.1 6667
+*/
 
 int	main(int ac, char **av)
 {
@@ -37,11 +47,75 @@ int	main(int ac, char **av)
 		// PREPARING A SOCKET STRUCTURE
 		struct sockaddr_in server_addr;
 
-		std::memset(&server_addr, 0, sizeof(server_addr));
+		memset(&server_addr, 0, sizeof(server_addr));
 		server_addr.sin_family = AF_INET;
 		server_addr.sin_port = htons(6667);
 		server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-		
+
+		std::cout << "Sin Family: " << server_addr.sin_family  << std::endl;
+		std::cout << "Sin Port: " << server_addr.sin_port  << std::endl;
+		std::cout << "Sin Address: " << server_addr.sin_addr.s_addr  << std::endl;
+		//Auto fill IP Address
+		struct addrinfo	hints;
+		struct addrinfo	*res;
+		struct addrinfo	*r;
+		int				status;
+		char			buffer[INET6_ADDRSTRLEN];
+
+		memset(&hints, 0, sizeof hints);
+		hints.ai_family = AF_UNSPEC;
+		hints.ai_socktype = SOCK_STREAM;
+		status = getaddrinfo(av[1], 0, &hints, &res);
+		if (status != 0)
+		{
+			std::cerr << "getaddrinfo error status: " << status << std::endl;
+			return (2);
+		}
+		std::cout << "IP Addr for :" << av[1] << std::endl;
+		r = res;
+		while (r)
+		{
+			if (r->ai_family == AF_INET)
+			{
+				struct sockaddr_in *ipv4 = (struct sockaddr_in *)r->ai_addr;
+				inet_ntop(r->ai_family, &(ipv4->sin_addr), buffer, sizeof buffer); // not allowable 
+				printf("IPv4: %s\n", buffer);
+			}
+			r = r->ai_next;
+		}
+		freeaddrinfo(res);
+
+		if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
+		{
+			perror("bind failed");
+			close(server_fd);
+			return (1);
+		}
+		if (listen(server_fd, 10) < 0)
+		{
+			perror("listen failed");
+			close(server_fd);
+			return (1);
+		}
+
+		std::cout << "Listening..." << std::endl;
+		struct sockaddr_in client_addr;
+		socklen_t client_len = sizeof client_addr;
+		int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
+		if (client_fd < 0)
+		{
+			perror("Accept failed");
+			close(server_fd);
+			return (1);
+		}
+		std::cout << "Client connected!" << std::endl;
+		int	bytes_read = read(client_fd, buffer, sizeof buffer - 1);
+		if (bytes_read > 0)
+		{
+			buffer[bytes_read] = '\0';
+			std::cout << "Received:" << buffer << std::endl;
+		}
+		close(client_fd);
 		close(server_fd);
 	}
 	catch (const std::exception &e)
@@ -51,3 +125,5 @@ int	main(int ac, char **av)
 	}
 	return (0);
 }
+
+
