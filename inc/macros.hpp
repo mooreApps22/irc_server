647bf9c75d3155f0e75a7b36bc1c2dbff5b21a48
Sergi Rodrigo <sergi.roderic@gmail.com>
2025-06-19 18:17:11 +0100

    wip

--- inc/macros.hpp
+++ inc/macros.hpp
@@ -1,2 +1,5 @@
 #pragma once
 
+#define ERR_UNKNOWNCOMMAND 		"421" // "<command> :Unknown command"
+#define ERR_ALREADYREGISTRED 	"462" // ":Unauthorized command (already registered)"
+#define ERR_PASSWDMISMATCH 		"464" //": Password incorrect"
