717ac1732622bd97ece26dc4163baef9c8ee469d
Sergi Rodrigo <sergi.roderic@gmail.com>
2025-07-02 14:31:04 +0200

    command USER refactored

---
+++ src/user.cpp
@@ -0,0 +1,40 @@
+#include "CommandHandler.hpp"
+#include "macros.hpp"
+#include "Logger.hpp"
+#include <string>
+
+void	CommandHandler::_userFp(const parsedMessage& parsedMsg) const
+{
+// 	Logger::log(INFO,  parsedMsg.command + " received.");
+	
+	std::string	userNickname = _srvAPI.getUserNickname();
+	std::string	command = parsedMsg.command;
+	std::string username;
+	
+	if(userNickname == "*")
+	{
+		_srvAPI.sendReply(ERR_NONICKNAMEGIVEN(userNickname, command));
+		return ;
+	}
+
+	if(_srvAPI.isUserRegistered())
+	{
+		_srvAPI.sendReply(ERR_ALREADYREGISTRED(userNickname));
+		return ;
+	}
+	
+	if(parsedMsg.params.size() < 4)
+	{
+		_srvAPI.sendReply(ERR_NEEDMOREPARAMS(userNickname, command));
+		return ;
+	}
+
+	username = parsedMsg.params.at(0);
+	_srvAPI.setUserUsername(username);
+	_srvAPI.setUserRegisteredStatus(true);
+
+ 	_srvAPI.sendReply(RPL_WELCOME(userNickname));
+	_srvAPI.sendReply(RPL_YOURHOST(userNickname));
+	_srvAPI.sendReply(RPL_CREATED(userNickname));
+	_srvAPI.sendReply(RPL_MYINFO(userNickname));
+}
