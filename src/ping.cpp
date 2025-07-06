f6ce02f2a00c4c8dd202547190837dcca2620e85
Sergi Rodrigo <sergi.roderic@gmail.com>
2025-07-06 11:58:45 +0200

    PING Refactored

---
+++ src/ping.cpp
@@ -0,0 +1,14 @@
+#include "CommandHandler.hpp"
+#include "macros.hpp"
+#include <string>
+
+void	CommandHandler::_pingFp(const parsedMessage& parsedMsg) const
+{
+	std::string replyMessage;
+	std::string userNickname = _srvAPI.getUserNickname();
+
+	if (parsedMsg.params.size() == 0)
+		_srvAPI.sendReply(ERR_NOORIGIN(userNickname));
+	else
+		_srvAPI.sendReply(PONG_RPL(parsedMsg.params.at(0)));
+}
