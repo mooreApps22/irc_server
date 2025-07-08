328105027619716a581413819e5fa5f23bb0ecde
Sergi Rodrigo <sergi.roderic@gmail.com>
2025-07-08 17:01:36 +0200

    CommandHandler refactored refactored

--- inc/CommandHandler.hpp
+++ inc/CommandHandler.hpp
@@ -1,37 +1,36 @@
 #pragma once
 # include "IServerAPI.hpp"
 # include "Parser.hpp"
-# include <iostream>
 # include <map>
 # include <string>
-
 class CommandHandler
 {
-	typedef std::map<std::string, void (CommandHandler::*)(parsed_message&)> commands;
-    private:
+	typedef std::map<std::string, void (CommandHandler::*)(const parsedMessage&) const> commands;
+	private:
 		IServerAPI&	_srvAPI;
 		commands	_commands;
 		
 		// Function pointers
-		void	_inviteFp(parsed_message& parsed_msg);
-		void	_joinFp(parsed_message& parsed_msg);
-		void	_kickFp(parsed_message& parsed_msg);
-		void	_modeFp(parsed_message& parsed_msg);
-		void	_nickFp(parsed_message& parsed_msg);
-		void	_passFp(parsed_message& parsed_msg);
-		void	_pingFp(parsed_message& parsed_msg);
-		void	_privMsgFp(parsed_message& parsed_msg);
-		void	_realFp(parsed_message& parsed_msg);
-		void	_topicFp(parsed_message& parsed_msg);
-		void	_userFp(parsed_message& parsed_msg);
-		void	_partFp(parsed_message& parsed_msg);
+		void	_inviteFp(const parsedMessage& parsedMsg) const;
+		void	_joinFp(const parsedMessage& parsedMsg) const;
+		void	_kickFp(const parsedMessage& parsedMsg) const;
+		void	_modeFp(const parsedMessage& parsedMsg) const;
+		void	_nickFp(const parsedMessage& parsedMsg) const;
+		void	_passFp(const parsedMessage& parsedMsg) const;
+		void	_pingFp(const parsedMessage& parsedMsg) const;
+		void	_privMsgFp(const parsedMessage& parsedMsg) const;
+		void	_topicFp(const parsedMessage& parsedMsg) const;
+		void	_userFp(const parsedMessage& parsedMsg) const;
+		void	_partFp(const parsedMessage& parsedMsg) const;
+		void	_quitFp(const parsedMessage& parsedMsg) const;
 
 		// Utils
-		bool				isNickUnique(const std::string nick);
-		const std::string 	build_reply(const std::string& arg1, const std::string& arg2, const std::string& arg3 = "", const std::string arg4 = "", const std::string arg5 = "", const std::string arg6 = "");
+		// const std::string 	buildReply(const std::string& arg1, const std::string& arg2, const std::string& arg3 = "", const std::string arg4 = "", const std::string arg5 = "", const std::string arg6 = "") const;
 
-    public:
+	public:
 		CommandHandler(IServerAPI& srvAPI);
-        ~CommandHandler();
-		void				execute(parsed_message& parsed_msg);
+		~CommandHandler();
+		void	execute(const parsedMessage& parsedMsg) const;
+		void 	disconnectClient() const;
+		void 	disconnectServer() const;
 };
--- src/CommandHandler.cpp
+++ src/CommandHandler.cpp
@@ -1,15 +1,14 @@
 #include "CommandHandler.hpp"
 #include "IServerAPI.hpp"
-#include "macros.hpp"
 #include "Logger.hpp"
-#include <vector>
+#include "macros.hpp"
+#include "User.hpp"
 #include <string>
 
 CommandHandler::CommandHandler(IServerAPI& srvAPI)
 	:	_srvAPI(srvAPI)
 {
-	Logger::log(INFO, "CommandHandler constructed.");
-
+	// Logger::log(INFO, "CommandHandler constructed.");
 	_commands["INVITE"]		= &CommandHandler::_inviteFp;
 	_commands["JOIN"]		= &CommandHandler::_joinFp;
 	_commands["KICK"]		= &CommandHandler::_kickFp;
@@ -21,201 +20,44 @@ CommandHandler::CommandHandler(IServerAPI& srvAPI)
 	_commands["TOPIC"] 		= &CommandHandler::_topicFp;
 	_commands["USER"] 		= &CommandHandler::_userFp;
 	_commands["PART"] 		= &CommandHandler::_partFp;
+	_commands["QUIT"] 		= &CommandHandler::_quitFp;
 }
 
 CommandHandler::~CommandHandler()
 {
-	Logger::log(INFO, "CommandHandler destructed.");
+	// Logger::log(INFO, "CommandHandler destructed.");
 }
 
-void	CommandHandler::execute(parsed_message& parsed_msg)
+void	CommandHandler::execute(const parsedMessage& parsedMsg) const
 {
-	Logger::log(INFO, "execute() called.");
-	const std::string command =  parsed_msg.command;
-	commands::iterator it = _commands.find(command);
+	const std::string command =  parsedMsg.command;
+	// Logger::log(INFO, "CommandHandler::execute() called.", command);
+
+	commands::const_iterator it = _commands.find(command);
 
 	if (it != _commands.end())
 	{
-		(this->*(it->second))(parsed_msg);
+		(this->*(it->second))(parsedMsg);
 	}
 	else
 	{
-		Logger::log(INFO, "Unknown Command.", command);
-
-		std::string reply_message;
-		std::string user_nickname = _srvAPI.getUserNick();
-
-		reply_message = build_reply(SERVER_NAME, ERR_UNKNOWNCOMMAND, user_nickname, command, "Unknown command");
-		_srvAPI.send_reply(reply_message);
+		// Logger::log(INFO, "Unknown Command.", command);
+		const std::string userNickname = _srvAPI.getUserNickname();
+		_srvAPI.sendReply(ERR_UNKNOWNCOMMAND(userNickname, command));
 	}
 }
 
-void	CommandHandler::_passFp(parsed_message& parsed_msg)
+void	CommandHandler::disconnectClient() const
 {
-	Logger::log(INFO,  parsed_msg.command + " received.");
+	std::string	userNickname = _srvAPI.getUserNickname();
+	std::string	userID = _srvAPI.getUserIdentifier();
 	
-	std::string	user_nickname = _srvAPI.getUserNick();
-	std::string	command = parsed_msg.command;
-	std::string reply_message;
-
-	if(_srvAPI.isUserRegistered())
-	{
-		reply_message = build_reply(SERVER_NAME, ERR_ALREADYREGISTRED, user_nickname, "Unauthorized command (already registered)");
-		_srvAPI.send_reply(reply_message);
-		return ;
-	}
-	
-	if(parsed_msg.params.size() != 1)
-	{
-		reply_message = build_reply(SERVER_NAME, ERR_PASSWDMISMATCH, user_nickname, command, "Not enough parameters");
-		_srvAPI.send_reply(reply_message);
-		_srvAPI.disconnectUser();
-		return ;
-	}
-
-	if(_srvAPI.isPasswordValid(parsed_msg.params.at(0)))
-		_srvAPI.setUserPasswordState(true);
-	else
-	{
-		reply_message = build_reply(SERVER_NAME, ERR_PASSWDMISMATCH, user_nickname, "Password incorrect");
-		_srvAPI.send_reply(reply_message);
-		_srvAPI.disconnectUser();
-	}
+	_srvAPI.sendMessageToChannelsWhereUser(EXIT_RPL(userID));
+	_srvAPI.disconnectClientFromServer();
 }
 
-void	CommandHandler::_pingFp(parsed_message& parsed_msg)
+void	CommandHandler::disconnectServer() const
 {
-	Logger::log(INFO,  parsed_msg.command + " received.");
-
-	std::string replyMessage;
-	std::string userNickname = _srvAPI.getUserNick();
-
-	if (parsed_msg.params.size() == 0)
-		replyMessage = build_reply(SERVER_NAME, ERR_NOORIGIN, userNickname, "No origin specified");
-	else
-		replyMessage = build_reply(SERVER_NAME, "PONG", SERVER_NAME, parsed_msg.params.at(0));
-	_srvAPI.send_reply(replyMessage);
-}
-
-void	CommandHandler::_userFp(parsed_message& parsed_msg)
-{
-	Logger::log(INFO,  parsed_msg.command + " received.");
-	
-	std::string	user_nickname = _srvAPI.getUserNick();
-	std::string	command = parsed_msg.command;
-	std::string reply_message;
-	std::string username;
-	// std::string realname;
-
-	
-	if(user_nickname == "*")
-	{
-		reply_message = build_reply(SERVER_NAME, ERR_NONICKNAMEGIVEN, user_nickname, command, "No nickname given");
-		_srvAPI.send_reply(reply_message);
-		_srvAPI.disconnectUser();
-		return ;
-	}
-
-	if(_srvAPI.isUserRegistered())
-	{
-		reply_message = build_reply(SERVER_NAME, ERR_ALREADYREGISTRED, user_nickname, "Unauthorized command (already registered)");
-		_srvAPI.send_reply(reply_message);
-		return ;
-	}
-	
-	if(parsed_msg.params.size() != 4)
-	{
-		reply_message = build_reply(SERVER_NAME, ERR_NEEDMOREPARAMS, user_nickname, command, "Not enough parameters");
-		_srvAPI.send_reply(reply_message);
-		_srvAPI.disconnectUser();
-		return ;
-	}
-
-	username = parsed_msg.params.at(0);
-	_srvAPI.setUserUsername(username);
-
-
-	_srvAPI.setUserRegisteredStatus(true);
-
-	std::string message = "Welcome to the Internet Relay Network ";
-	message += _srvAPI.getUserIdentifier();
-	reply_message = build_reply(SERVER_NAME, RPL_WELCOME, user_nickname, message);
-	_srvAPI.send_reply(reply_message);
-
-	message = "Your host is ";
-	message += SERVER_NAME;
-	message += ", running version ";
-	message += VERSION;
-	reply_message = build_reply(SERVER_NAME, RPL_YOURHOST, user_nickname, message);
-	_srvAPI.send_reply(reply_message);
-
-	message = "This server was created ";
-	message += CREATION_DATE;
-	reply_message = build_reply(SERVER_NAME, RPL_CREATED, user_nickname, message);
-	_srvAPI.send_reply(reply_message);
-
-	message = SERVER_NAME;
-	message += SPACE;
-	message += VERSION;
-	message += SPACE;
-	message += USER_MODES;
-	message += SPACE;
-	message += CHANNEL_MODES;
-	reply_message = build_reply(SERVER_NAME, RPL_MYINFO, user_nickname, message);
-	_srvAPI.send_reply(reply_message);
-}
-
-bool	CommandHandler::isNickUnique(const std::string nick)
-{
-	for (usrsIt it = _srvAPI.getUsersBegin(); it != _srvAPI.getUsersEnd(); it++)
-	{
-		if(it->second->getNickname() == nick)
-			return false;
-	}
-	return true;
-}
-
-const std::string CommandHandler::build_reply(const std::string& arg1, const std::string& arg2, const std::string& arg3, const std::string arg4, const std::string arg5, const std::string arg6)
-{
-	std::string reply_message = COLON;
-	reply_message += arg1;
-
-	reply_message += SPACE;
-	if (arg2.size() == 0)
-		reply_message += COLON;
-	reply_message += arg2;
-
-	if (arg3.size() > 0)
-	{
-		reply_message += SPACE;
-		if (arg4.size() == 0)
-			reply_message += COLON;
-		reply_message += arg3;
-	}
-
-	if (arg4.size() > 0)
-	{
-		reply_message += SPACE;
-		if (arg5.size() == 0)
-			reply_message += COLON;
-		reply_message += arg4;
-	}
-
-	if (arg5.size() > 0)
-	{
-		reply_message += SPACE;
-		if (arg6.size() == 0)
-			reply_message += COLON;
-		reply_message += arg5;
-	}
-	
-	if (arg6.size() > 0)
-	{
-		reply_message += SPACE;
-		reply_message += COLON;
-		reply_message += arg6;
-	}
-	if (arg2 != "PONG")
-		std::cout << "Built message: " << reply_message << std::endl;
-	return reply_message;
+	_srvAPI.sendToAll(SERVER_ERROR_RPL);
+	_srvAPI.disconnectAllClientsFromServer();
 }
